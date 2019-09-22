4e317e8824189037c8f13b4b4b32d406acc768cc
Adrian Carpenter <adriatic.c@gmail.com>
2019-09-22 09:46:51 -0700

    Add json feed data

--- src/Widget.vue
+++ src/Widget.vue
@@ -8,23 +8,16 @@
       </div>
       <div class="deadline">
         <div class="title"><span>{{ content.deadline }}</span></div>
-        <div class="carbon"><span>{{ tonnage }}</span></div>
-        <div class="time"><span>{{ time }}</span></div>
+        <div class="carbon"><span>{{ co2Budget.toLocaleString() }} TONS</span></div>
+        <div class="time"><span>{{ years }} YEARS {{ days }} DAYS {{ hours | pad }}:{{ minutes | pad }}:{{ seconds | pad }}</span></div>
       </div>
       <div class="lifeline">
         <div class="title"><span>{{ content.lifeline }}</span></div>
         <div class="feed">
-          <!--
-            <span>{{ content.feed }}&nbsp;{{ content.feed }}</span>
-          -->
-          <span class="lead">{{ content.feed }}</span>
-          <span class="follow">{{ content.feed }}</span>
+          <span class="lead">{{ content.feed }}&nbsp;</span>
+          <span class="follow">{{ content.feed }}&nbsp;</span>
         </div>
       </div>
-      <!--
-      <h1 :style="{color: imp(deadline)}">CLIMATECLOCK</h1>
-      <h1 :style="{color: imp(lifeline)}" @click="change">{{ tagline }}</h1>
-      -->
     </div>
   </div>
 </template>
@@ -32,41 +25,58 @@
 
 <script>
 import content from './content.aml'
+import countdown from 'countdown/countdown.js'
 
-const lifeline = '#b0d155'
-const deadline = '#c42a30'
+//const lifeline = '#b0d155'
+//const deadline = '#c42a30'
 
 export default {
   name: 'climate-clock-widget',
   data: () => ({
     content: content,
     tagline: 'this is the second line',
-    tonnage: 'manny ton',
     time: '8 years',
+    co2Budget: 420000000000,
+    tonsPerSecond: 1332,
+    startdate: new Date("Oct08, 2018 00:00:00"),
+    deadline: new Date("Jun10, 2028 00:00:00"),
+    interval: 100,
+    years: null,
+    days: null,
+    hours: null,
+    minutes: null,
+    seconds: null,
   }),
   props: {
-    deadline: {type: String, default: deadline},
-    lifeline: {type: String, default: lifeline},
     size: {type: Number, default: 30},
+  },
+  filters: {
+    pad(n) {
+      return ('00' + n).slice(-2)
+    }
   },
   methods: {
     imp(value) { 
       return value.toString() + '!important' 
     },
-    smallFont() {
-      return `${this.size * .3}px!important`
-    },
-    bigFont() {
-      return this.size + 'px'
-    },
-    change() {
-      this.tagline = 'clicked'
-      setTimeout(this.reset, 1000)
-    },
-    reset() {
-      this.tagline = 'the 2nd line'
-    },
+    updateClock() {
+      let timeleft = countdown(this.deadline, new Date(),
+        countdown.YEARS | countdown.DAYS | countdown.HOURS | countdown.MINUTES | countdown.SECONDS)
+      this.seconds = timeleft.seconds
+      this.minutes = timeleft.minutes
+      this.hours = timeleft.hours
+      this.days = timeleft.days
+      this.years = timeleft.years
+      this.co2Budget -= Math.floor(this.tonsPerSecond * (this.interval / 1000))
+    }
   },
+  created() {
+    let now = (new Date()).getTime()
+    let start = this.startdate.getTime()
+    let seconds = Math.floor((now - start)/1000)
+    this.co2Budget -= seconds * this.tonsPerSecond
+    setInterval(() => this.updateClock(), this.interval)
+  }
 }
 </script>
 
@@ -95,32 +105,15 @@ $duration: 20s;
   font-weight: 600;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  div span {
+  span {
     text-align: center;
-    text-transform: uppercase;
     letter-spacing: .04rem;
+    white-space: nowrap;
   }
   .container {
     height: 105px;
     display: flex;
     flex-direction: column;
-  }
-  .header {
-    flex: $short 0 0;
-    > div {
-      background-color: $light;
-      color: $dark;
-      > span {
-        font-weight: 800;
-      }
-    }
-    .title {
-      color: $dark;
-      background-color: white;
-    }
-  }
-  .carbon, .time, .feed {
-    border-left: 1px solid $light;
   }
   .header, .deadline, .lifeline {
     display: flex;
@@ -131,34 +124,56 @@ $duration: 20s;
       justify-content: center;
     }
   }
-  .deadline {
-    margin-bottom: .2rem;
-  }
   .deadline, .lifeline {
     background-color: $dark;
     color: $light;
     flex: $tall 0 0;
   }
-  .title {
-    flex: $narrow 0 0;
+  .deadline {
+    margin-bottom: .2rem;
+  }
+  .header {
+    flex: $short 0 0;
+    background-color: $light;
+    div {
+      color: $dark;
+      font-weight: 800;
+    }
+    .title {
+      color: $dark;
+      background-color: white;
+    }
+  }
+  .carbon, .time, .title {
+    span {
+      text-transform: uppercase;
+    }
+  }
+  .carbon, .time, .title, .feed {
+    overflow: hidden;
+  }
+  .carbon, .time, .feed {
+    border-left: 1px solid $light;
   }
   .carbon, .time {
     flex: $wide 0 0;
     color: $accent;
+  }
+  .title {
+    flex: $narrow 0 0;
   }
   .feed {
     flex: $wide * 2 0 0;
     overflow: hidden;
     color: $secondary;
     position: relative;
-    span.lead {
-      white-space: nowrap;
+    text-transform: none;
+    .lead {
       position: absolute;
       transform: translateX(0%);
       animation: feed-lead $duration linear infinite;
     }
-    span.follow {
-      white-space: nowrap;
+    .follow {
       position: absolute;
       transform: translateX(100%);
       animation: feed-follow $duration linear infinite;
