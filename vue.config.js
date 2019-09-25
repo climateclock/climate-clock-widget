3d028cc5f415b2da510078491cc4c8bd31ec77f9
Adrian Carpenter <adriatic.c@gmail.com>
2019-09-24 20:26:32 -0700

    Don't produce an index.html except for development

--- vue.config.js
+++ vue.config.js
@@ -2,10 +2,13 @@ module.exports = {
   chainWebpack: config => {
     // Don't split vendor and app js
     config.optimization.splitChunks(false)
-    // Don't generate an index.html
-    //config.plugins.delete('html')
-    //config.plugins.delete('preload')
-    //config.plugins.delete('prefetch')
+    // Don't generate an index.html in production
+    if (process.env.NODE_ENV == 'production') {
+      config.plugins.delete('html')
+      config.plugins.delete('preload')
+      config.plugins.delete('prefetch')
+      config.plugins.delete('copy')
+    }
   },
   configureWebpack: {
     output: {filename: 'widget-v1.js'}
