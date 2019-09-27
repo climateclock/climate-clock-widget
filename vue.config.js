b9c11b66b5d2fd113517700d843a547fa9b35e89
Adrian Carpenter <adriatic.c@gmail.com>
2019-09-26 23:18:35 -0700

    Expose process.env.NODE_ENV to .scss as $NODE_ENV

--- vue.config.js
+++ vue.config.js
@@ -13,8 +13,14 @@ module.exports = {
   configureWebpack: {
     output: {filename: 'widget-v1.js'}
   },
-  // Don't produce a separate css file
-  css: {extract: false},
+  css: {
+    // Don't produce a separate css file
+    extract: false,
+    loaderOptions: {
+      // Expose NODE_ENV to scss
+      sass: {data: '$NODE_ENV: ' + process.env.NODE_ENV + ';'}
+    }
+  },
   // Don't produce a sourcemap
   productionSourceMap: false,
   // Use one static name
