e8dcb324a6e8088e734953313657f0c355b828a5
Adrian Carpenter <adriatic.c@gmail.com>
2019-09-28 03:03:21 -0700

    Re-add debounce

--- src/main.js
+++ src/main.js
@@ -8,11 +8,13 @@ import VueCustomElement from 'vue-custom-element'
 import browserDetect from 'vue-browser-detect-plugin'
 import axios from 'axios'
 import countdown from 'countdown'
+import debounce from 'lodash.debounce'
 
 Vue.prototype.$http = axios
 Vue.mixin({
   methods: {
     countdown: countdown,
+    debounce: debounce,
   },
 })
 Vue.config.ignoredElements = [/^ccw-/]
