a4da60595ff3cca16ac03b4c68f66749564b79e8
Adrian Carpenter <adriatic.c@gmail.com>
2020-04-05 01:59:56 -0700

    Add earth icon and SVG loader

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,12 @@ module.exports = {
   chainWebpack: config => {
     // Don't split vendor and app js
     config.optimization.splitChunks(false)
+    // Inline SVGs
+    config.module
+      .rule('vue')
+      .use('vue-svg-inline-loader')
+        .loader('vue-svg-inline-loader')
+        .options({})
   },
   configureWebpack: {
     output: {filename: 'widget-v1jw.js'}
